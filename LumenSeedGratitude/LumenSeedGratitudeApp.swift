16df1eb72d16400dec62607150150e4d8a0163ba
e.shirashiyani <e.shirashiyani>
2024-12-03 21:50:35 +0330

    Initial Commit

---
+++ LumenSeedGratitude/LumenSeedGratitudeApp.swift
@@ -0,0 +1,32 @@
+//
+//  LumenSeedGratitudeApp.swift
+//  LumenSeedGratitude
+//
+//  Created by e.shirashiyani on 12/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LumenSeedGratitudeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
